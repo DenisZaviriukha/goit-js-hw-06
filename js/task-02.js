db1ba6322d46ef0861fe032f685d73e77ba03484
DenisZaviriukha <dv.zavirukha@gmail.com>
2023-02-09 01:20:00 +0200

    Done 2nd ex

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const arrayOfDoneItems = []
+
+// 1. 2. 3.
+
+const itemsAddingValueAndClass = ingredients.forEach((ingredientsValue) => {
+  const itemEl = document.createElement('li')
+  itemEl.textContent = ingredientsValue
+  itemEl.classList.add('item')
+  arrayOfDoneItems.push(itemEl)
+})
+
+// 4.
+
+const listEl = document.querySelector('#ingredients')
+  
+listEl.append(...arrayOfDoneItems)

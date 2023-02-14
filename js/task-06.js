b0ae627a98e939573c7480e93a55f5cb4833ae8a
DenisZaviriukha <dv.zavirukha@gmail.com>
2023-02-14 14:33:21 +0200

    6th corrected

--- js/task-06.js
+++ js/task-06.js
@@ -2,14 +2,16 @@ const input = document.querySelector('#validation-input')
 
 const onInputBlur = (event) => {
 
-    const inputLength = `${event.currentTarget.getAttribute("data-length")}`
-    
-    const dataLength = `${event.currentTarget.value.trim().length}`
+    const inputLength = Number(event.currentTarget.getAttribute("data-length"))
+
+    const dataLength = event.currentTarget.value.trim().length
 
     if (inputLength !== dataLength) {
+        event.currentTarget.classList.remove('valid')
         event.currentTarget.classList.add('invalid')
         return
-    }    
+    }
+    event.currentTarget.classList.remove('invalid')
     event.currentTarget.classList.add('valid')
 }
 

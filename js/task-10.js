e9e755bc29e05f57fda53fb39f64c7a0704c204b
DenisZaviriukha <dv.zavirukha@gmail.com>
2023-02-19 18:40:00 +0200

    done 10th

--- js/task-10.js
+++ js/task-10.js
@@ -1,30 +1,37 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
-// let number = document.querySelector('#controls>input')
-// const creteButton = document.querySelector('button[data-create]')
-// const destroyButton = document.querySelector('button[data-destroy]')
+const createBtn = document.querySelector('[data-create]')
+const destroyBtn = document.querySelector('[data-destroy]')
+const boxesWrapper = document.querySelector('#boxes')
+const amount = document.querySelector('#controls>input')
+let boxesArr = []
 
-// const boxes = document.querySelector('div#boxes')
 
-// const onCreateButton = (amount) => {
-//   amount = number
-//   // for (let i = 0; i <= amount; i+=1) {
-    
-//   // }
-//   document.createElement('div')
-//   let ccc = docume
-//     div.style.height = "30px"
-//     div.style.width = "30px"
-//     div.style.color = getRandomHexColor()
-// }
+const onCreateButton = () => { 
+  const owo = boxesArr.length
+  const uwu = Number(amount.value) + boxesArr.length
+  for (let i = owo; i < uwu; i += 1) {
+    const box = document.createElement("div")
+    box.style.backgroundColor = getRandomHexColor()
+    box.style.width = `${30 + (10 * i)}px`
+    box.style.height = `${30 + (10 * i)}px`
+    box.textContent = getRandomHexColor()
+    if (box.style.backgroundColor){
+      boxesArr.push(box)
+    }
+  }
+  boxesWrapper.append(...boxesArr)
+}
 
-// const onDestroyBoxes = () => {
+const onDestroyButton = () => {
+  if (boxesWrapper.innerHTML) {
+  boxesWrapper.innerHTML=''
+  boxesArr = []
+  }
   
-// }
+}
 
-// creteButton.addEventListener('click', onCreateButton)
-
-// destroyButton.addEventListener('click', onDestroyBoxes)
-
+createBtn.addEventListener('click', onCreateButton)
+destroyBtn.addEventListener('click', onDestroyButton)
